bf869afc567a1886af9081e3673d0370748c51bb
Jared Lindquist <jared.lindquist1985@gmail.com>
2022-12-22 08:59:25 -0600

    .env file set up, edits to database.sql file to include instructions for creating the 'recipe' table.

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
-
+Create a new database called `prime_app`
 -- USER is a reserved keyword with Postgres
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
@@ -7,4 +7,15 @@ CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
+);
+
+CREATE TABLE "recipes" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" SERIAL FOREIGN KEY,
+    "coffee" VARCHAR(255) NOT NULL,
+    "roast_level" VARCHAR(255) NOT NULL,
+    "brew_method" VARCHAR(255) NOT NULL,
+    "input" INT,
+    "output" INT,
+    "comments" VARCHAR (255)
 );
