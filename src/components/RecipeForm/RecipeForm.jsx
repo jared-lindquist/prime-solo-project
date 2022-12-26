1aa4f8250ce011a87a6ebefd804688f31dd9798e
Jared Lindquist <jared.lindquist1985@gmail.com>
2022-12-26 16:28:49 -0600

    used MUI to build a basic wire-frame of the RecipeForm input form to see what it could look like

--- src/components/RecipeForm/RecipeForm.jsx
+++ src/components/RecipeForm/RecipeForm.jsx
@@ -1,18 +1,79 @@
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
+
 function RecipeForm() {
 
     return (
+        <Box
+        component="form"
+        sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+        >   
         <div>
-            recipe form goes here. include inputs for
-            give this recipe a title
-            what coffee did you use?
-            light/medium/dark roast?
-            brew method (drop down)
-            input (how much coffee in grams did you start with)
-            output (how much brewed coffee did you end up with)
-            tasting notes/comments
-            <button>Save this brew</button>
-            clicking save navigates to UserRecipes list view
+            <TextField
+                required
+                id="recipe-title-input"
+                label="Recipe Title"
+                variant="filled"
+                type="text"
+            />
+            <br/>
+            <TextField
+                required
+                id="coffee-input"
+                label="Coffee Used"
+                variant="filled"
+                type="text"
+            />
+            <br/>
+            <TextField
+                required
+                id="roast-level"
+                label="Light/Medium/Dark Roast?"
+                type="text"
+                variant="filled"
+            />
+            <br/>
+            <TextField
+                required
+                id="input"
+                label="Input in Grams"
+                variant="filled"
+                type="number"
+            />
+            <br/>
+            <TextField
+                required
+                id="output"
+                label="Output in Grams"
+                type="number"
+                variant="filled"
+            />
+            <br/>
+            <TextField
+                id="comments"
+                label="Tasting Notes/Comments"
+                type="text"
+                variant="standard"
+                multiline
+                maxRows={4}
+            />
+            <br/>
+            <br/>
+            <Button
+                variant="contained"
+            >
+            Save this brew
+            </Button>
+            <br/>
+            clicking save navigates to UserRecipes list 
+            and sends POST request to DB.
         </div>
+</Box> 
     )
 }
 
